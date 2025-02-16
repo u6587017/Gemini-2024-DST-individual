921ced357b077c879fdc5de8b88e8b128cf39ca3
Thanawich Juengkijthanawat <thanawich.jue@student.mahidol.edu>
2025-02-16 15:25:34 +0700

    Update App.java

--- Thanawich/HolidayTravelVehicles/src/App.java
+++ Thanawich/HolidayTravelVehicles/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Vehicle vehicle1 = new Vehicle(001, "F1", "Zx-320", "Bruno Fernandes", 1000);
+
+        vehicle1.getInfo();
     }
 }
