13befae98be9bf34acf0de87c40800c50088f940
chocody <108443531+chocody@users.noreply.github.com>
2025-02-16 19:59:40 +0700

    Delete Suntipap/HolidayTravelVehicles directory

--- Suntipap/HolidayTravelVehicles/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        Company HTV = new Company(1,"Holiday_Travel_Vehicles",01234567);
-        Vehicle RV = HTV.createVehicle(01,"Airstream basecamp","Basecamp 20",2,"Airstream",40000);
-        RV.print_vehicle();
-    }
-}
